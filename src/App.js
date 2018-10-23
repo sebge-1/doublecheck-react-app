039ad8fd00e93250d6671714a0f8d450c1c5e354
Sebastian <sebgertz@gmail.com>
2018-10-23 18:29:28 -0400

    feat: added Link Component to App landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom' 
+import { BrowserRouter, Link } from 'react-router-dom'
 import NavBar from './Components/NavBar.js'
 import './App.css';
 
@@ -10,17 +10,24 @@ class App extends Component {
         <BrowserRouter>
           <NavBar />
         </BrowserRouter>
-        <header className="App-header">
-          <h1>
-            Welcome to DoubleCheck!
-          </h1>
-          <hr/>
-          <h4>
-            Are you communicating the right message?
-          </h4>
-          <p>
-            Paste your texts and tweets into DoubleCheck to find out.
-          </p>
+          <header className="App-header">
+            <h1>
+              Welcome to DoubleCheck!
+            </h1>
+            <hr/>
+            <h4>
+              Are you sending the right message?
+            </h4>
+            <p>
+              Paste your texts and tweets into DoubleCheck to find out.
+            </p>
+            <BrowserRouter>
+              <Link to="/projects/new">
+                <button type="button">
+                  Get Started
+                </button>
+              </Link>
+          </BrowserRouter>
         </header>
       </div>
     );
