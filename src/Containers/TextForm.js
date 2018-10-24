f435c1120b4767e1b88c644702dcf76763c72e22
Sebastian <sebgertz@gmail.com>
2018-10-24 17:05:15 -0400

    feat: built out TextForm component to track internal state, stubbed out onSubmit function

---
+++ src/Containers/TextForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addProject } from '../Actions/index.js';
+
+class TextForm extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    const { addProject, history } = this.props;
+    addProject(this.state);
+    history.push('/result')
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  render(){
+    return (
+      <form style={{ marginTop: '16px' }} onSubmit={this.handleOnSubmit} >
+        <textarea name="text" cols="40" rows="5"
+          onChange={this.handleOnChange}
+          placeholder="Paste text here">
+        </textarea>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default connect(null, { addProject })(TextForm)
