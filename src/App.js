e7c56b8d952ffb2bf6bda2a3f7e58f5877d00227
Sebastian <sebgertz@gmail.com>
2018-10-24 13:07:09 -0400

    feat: set up Route to form component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link } from 'react-router-dom'
-import NavBar from './Components/NavBar.js'
+import {Link, BrowserRouter as Router, Route
+} from 'react-router-dom';
+import TextForm from './Containers/TextForm.js'
 import { FaCheckDouble } from 'react-icons/fa'
-import { Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <NavBar />
-        </BrowserRouter>
-          <header className="App-header">
-            <h1>
-              Welcome to DoubleCheck!<br/>
-              <FaCheckDouble />
-            </h1>
-            <hr/>
-            <h4>
-              Are you sending the right message?
-            </h4>
-            <p>
-              Paste your texts and tweets into DoubleCheck to find out.
-            </p>
-            <BrowserRouter>
+        <header className="App-header">
+          <h1>
+            Welcome to DoubleCheck!<br/>
+            <FaCheckDouble />
+          </h1>
+          <hr/>
+          <h4>
+            Are you sending the right message?
+          </h4>
+          <p>
+            Paste your texts and tweets into DoubleCheck to find out.
+          </p>
+          <Router>
+            <div><Route path='/projects/new' component={TextForm}/>
               <Link to="/projects/new">
-                <Button>
-                  Get Started
-                </Button>
+              <button>
+                Get Started
+              </button>
               </Link>
-          </BrowserRouter>
-        </header>
-      </div>
+            </div>
+          </Router>
+      </header>
+    </div>
     );
   }
 }
