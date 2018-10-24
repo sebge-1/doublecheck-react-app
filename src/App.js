47a04029b36a5e16b17a1aad7b72a710dc4bae39
Sebastian <sebgertz@gmail.com>
2018-10-24 16:40:00 -0400

    refactor: changed App component to handle Switch routing only

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
-import {Link, BrowserRouter as Router, Route
+import {Switch, BrowserRouter as Router, Route
 } from 'react-router-dom';
 import TextForm from './Containers/TextForm.js'
-import { FaCheckDouble } from 'react-icons/fa'
+import Home from './Components/Home.js'
+import Projects from './Containers/Projects.js'
+import Result from './Components/Result.js'                                                                                                                          
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Welcome to DoubleCheck!<br/>
-            <FaCheckDouble />
-          </h1>
-          <hr/>
-          <h4>
-            Are you sending the right message?
-          </h4>
-          <p>
-            Paste your texts and tweets into DoubleCheck to find out.
-          </p>
-          <Router>
-            <div><Route path='/projects/new' component={TextForm}/>
-              <Link to="/projects/new">
-              <button>
-                Get Started
-              </button>
-              </Link>
-            </div>
-          </Router>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/result' component={Result} />
+          <Route exact path='/projects/new' component={TextForm}/>
+        </Switch>
+      </Router>
     );
   }
 }
