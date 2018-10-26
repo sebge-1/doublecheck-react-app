c5556c211268d0d9c14684be6ef5b7c125342a8c
Sebastian <sebgertz@gmail.com>
2018-10-26 18:22:55 -0400

    refactor: wrapped post request into Promise to handle asynchronicity

--- src/tone.js
+++ src/tone.js
@@ -1,23 +1,26 @@
-import { addProject } from './Actions/index.js'
-
 export function toneAnalyzer(project) {
-  let request = require("request");
   let toneUsername= "5be14bed-d818-4bd1-b4ca-e8b09dcc73b8";
   let tonePassword= "THTqBjmyU6k4";
   let toneUrl = "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21";
-
   let data = {};
   data.text = project.text
+  let request = require("request");
 
-  request.post({
-    url: toneUrl,
-    json: data,
-    auth: {
-        user: toneUsername,
-        pass: tonePassword
+  return new Promise(
+    (resolve, reject) => {
+     request.post(
+       {
+         url: toneUrl,
+         json: data,
+         auth: {
+           user: toneUsername,
+           pass: tonePassword
+         }
+       }, function(err, res, data) {
+            if (err) reject(err);
+
+            resolve(data);
+          })
     }
-
-  }, function (err, response, body){
-      console.log(body);
-  })
+  );
 }
