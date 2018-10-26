50db4dc8b84b761fa80781a27cc68c0a8e08c231
Sebastian <sebgertz@gmail.com>
2018-10-26 18:25:00 -0400

    feat: Result components can iterate over tone and sentence analysis from Watson API and displays info

---
+++ src/Components/Result.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Result = ({ project }) =>
+  <div>
+    <h1>Results</h1>
+    <p>Passage: {project.text}</p>
+    <h4>Summary</h4>
+    <ol>
+      {project.result.document_tone.tones.map(tone => <li><em>{tone.tone_name}</em><p> Score: {tone.score}</p></li>)}
+    </ol>
+    <h4>Detailed Analysis</h4>
+    <ul>
+      {project.result.sentences_tone.map(
+        sentence =>
+        <div>
+          <li><em>Sentence {sentence.sentence_id + 1}: {sentence.text}</em>
+          </li>
+          <ol>{sentence.tones.map(tone =>
+            <li><em>{tone.tone_name}</em>
+              <p>Score: {tone.score}</p>
+            </li>
+          )}
+        </ol>
+      </div>
+        )}
+    </ul>
+    <button>
+      Save Project
+    </button>
+  </div>
+
+const mapStateToProps = (state, ownProps) => {
+  const project = state.projects.find(project => project.id === +ownProps.match.params.projectId)
+
+  if (project) {
+    return { project }
+  } else {
+    return { project: {} }
+  }
+}
+
+export default connect(mapStateToProps)(Result);
