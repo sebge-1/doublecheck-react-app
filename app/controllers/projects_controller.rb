f71f5aa41be2676dd256b056e230b1532c278bf2
Sebastian <sebgertz@gmail.com>
2018-11-01 15:09:39 -0400

    added create route to enable save action on client side

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,14 @@
 class ProjectsController < ApplicationController
+  def index
+    render json: Project.all
+  end
+
+  def create
+    project = Project.create(project_params)
+    render json: project, status: 201
+  end
+
+  def project_params
+    params.permit(:id, :text, :result)
+  end
 end
