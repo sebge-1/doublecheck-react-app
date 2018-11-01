dcf13e1f4f58d1effa7252e217e7d05c2033535b
Sebastian <sebgertz@gmail.com>
2018-11-01 15:23:26 -0400

    feat: added FETCH_PROJECTS action type in projects reducer

--- client/src/Reducers/projects.js
+++ client/src/Reducers/projects.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'ADD_PROJECT':
       return [ ...state, action.project ];
+    case 'FETCH_PROJECTS':
+      return {projets: action.payload};
     default: // need this for default case
       return state;
     }
