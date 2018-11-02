f8193c176e822a902e8698d3249a2fe71ad3cdc3
Sebastian <sebgertz@gmail.com>
2018-11-02 17:11:22 -0400

    feat: App component imports fetch function, calls it after mounting and connects to Redux state with mapDispatch and mapState functions

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,22 @@ import {Switch, BrowserRouter as Router, Route
 import TextForm from './Containers/TextForm.js'
 import Home from './Components/Home.js'
 import Projects from './Containers/Projects.js'
-import Result from './Components/Result.js'                                                                                                                          
+import Result from './Components/Result.js'
+import { fetchProjects } from './Actions/index.js'
+import { connect } from "react-redux";                                                                                                                  
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchProjects();
+  }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/projects' render={(props) => <Projects {...props} projects={this.props.projectList} />} />
           <Route path='/projects/:projectId/result' component={Result} />
           <Route exact path='/projects/new' component={TextForm}/>
         </Switch>
@@ -22,4 +28,14 @@ class App extends Component {
   }
 }
 
-export default App;
+
+// mapStateToProps is a shorthand to access one slice of the global state of the app (kept in the redux store) via props
+const mapStateToProps = state => {
+  return { projectList: state.projects }
+}
+// mapDispatchToProps is a shorthand that lets the app component dispatch an action via props
+ const mapDispatchToProps = dispatch => ({
+  fetchProjects: () => dispatch(fetchProjects())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
