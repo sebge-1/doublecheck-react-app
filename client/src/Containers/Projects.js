e37812f42d2901fcdd13c0f332ba45bad84c501a
Sebastian <sebgertz@gmail.com>
2018-11-05 16:58:17 -0500

    added internal state to Component to check props are ready to display in DOM; added checkLoadState and componentDidMount functions

--- client/src/Containers/Projects.js
+++ client/src/Containers/Projects.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 export default class Projects extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loadMessage: "...Loading"
+    };
+  }
+
+  checkLoadState = () => {
+    if (this.state.loadMessage === "...Loading") {
+      if (this.props.projects.length === 0) {
+        this.setState({loadMessage: "Nothing to show yet. Try creating and saving a project first"})
+      } else {
+      this.setState({loadMessage: "Something went wrong. Try to refresh the page"})
+      }
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(this.checkLoadState, 3000);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.projects.length !== 0) {
+      this.setState({loadMessage: "Results:"});
+    }
+  }
+
   render() {
+
     return (
-      <h1>Projects</h1>
+      <div>
+        <h1>{this.state.loadMessage}</h1>
+      </div>
     )
   }
 }
