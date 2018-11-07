4b41a3acddfe5739dd5dd530269a237177c8abdb
Sebastian <sebgertz@gmail.com>
2018-11-06 23:33:52 -0500

    refactor: changed Results dispaly to match Project structure

--- client/src/Components/Result.js
+++ client/src/Components/Result.js
@@ -1,42 +1,66 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import {Link} from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
-const Result = ({ project }) =>
-  <div>
-    <h1>Results</h1>
-    <p>Passage: {project.text}</p>
-    <h4>Summary</h4>
-    <ol>
-      {project.result.document_tone.tones.map(tone =>
-        <li>
-          <em>{tone.tone_name}</em>
-          <p> Score: {tone.score}</p>
-        </li>)}
-    </ol>
-    <h4>Detailed Analysis</h4>
-    <ul>
-      {project.result.sentences_tone.map(
-        sentence =>
-        <div>
+class Result extends Component {
+  saveProject = () => {
+    const { history } = this.props;
+    fetch('/api/save', {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(this.props.project), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(function(response) {
+        if(response.ok) {
+          history.push("/projects")
+        }
+    // (res => res.json())
+    // .then(response => console.log('Success:', JSON.stringify(response)))
+    // .catch(error => console.error('Error:', error));
+  })
+  }
+
+  render() {
+    console.log(this.props.project)
+    return(
+    <div>
+      <h1>Results</h1>
+      <p>Passage: {this.props.project.text}</p>
+      <h4>Summary</h4>
+      <ol>
+        {this.props.project.tones.map(tone =>
           <li>
-            <em>Sentence {sentence.sentence_id + 1}: {sentence.text}</em>
-          </li>
-          <ol>{sentence.tones.map(tone =>
+            <em>{tone.tone_name}</em>
+            <p> Score: {tone.score}</p>
+          </li>)}
+      </ol>
+      <h4>Detailed Analysis</h4>
+      <ul>
+        {this.props.project.sentences.map(
+          sentence =>
+          <div>
             <li>
-              <em>{tone.tone_name}</em>
-              <p>Score: {tone.score}</p>
+              <em>Sentence {sentence.sentence_id + 1}: {sentence.text}</em>
             </li>
+            <ol>{sentence.tones.map(tone =>
+              <li>
+                <em>{tone.tone_name}</em>
+                <p>Score: {tone.score}</p>
+              </li>
+            )}
+          </ol>
+        </div>
           )}
-        </ol>
-      </div>
-        )}
-    </ul>
-    <button>
-      Save project
-    </button>
-  </div>
+      </ul>
+      <button onClick={this.saveProject}>
+        Save project
+      </button>
+    </div>
+    )
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const project = state.projects.find(project => project.idx === +ownProps.match.params.projectId)
