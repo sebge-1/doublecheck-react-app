caef5f6f17d457ae05c45df2d6f7b9c33c50143e
Sebastian <sebgertz@gmail.com>
2018-11-08 21:09:05 -0500

    refactor: changed Home to stateless component

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import { FaCheckDouble } from 'react-icons/fa'
+import {Button} from 'reactstrap'
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Welcome to DoubleCheck!<br/>
-            <FaCheckDouble />
-          </h1>
-          <hr/>
-          <h4>
-            Are you sending the right message?
-          </h4>
-          <p>
-            Paste your texts and tweets into DoubleCheck to find out.
-          </p>
-            <Link to="/projects/new">
-              <button>
-                Get Started
-              </button>
-            </Link>
-      </header>
-    </div>
-    )
-  }
-}
+const Home = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>
+        Welcome to DoubleCheck!</h1>
+        <br/>
+        <FaCheckDouble />
+      <hr/>
+      <h4>
+        Are you sending the right message?
+      </h4>
+      <p>
+        Paste your texts and tweets into DoubleCheck to find out.
+      </p>
+      <Link to="/projects/new">
+        <Button>
+          Get Started
+        </Button>
+      </Link>
+    </header>
+  </div>
+)
+export default Home;
