8764ad7432ccf77e2f4781fdab6dc800d44d6490
Sebastian <sebgertz@gmail.com>
2018-11-08 20:54:10 -0500

    build: imported bootstrap css

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './Reducers/index'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
