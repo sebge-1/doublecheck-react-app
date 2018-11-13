4ee9204e706ef39439e187a432907c76f8bce5c7
Sebastian <sebgertz@gmail.com>
2018-11-12 22:20:35 -0500

    chore: cleaned up indentations

--- client/src/Components/Result.js
+++ client/src/Components/Result.js
@@ -7,36 +7,36 @@ import '../stylesheets/Result.css'
 export default class Result extends Component {
   render() {
     return(
-  <div className="App-header">
-      <h2 className='cardTitle'><em>{this.props.project.title}</em></h2>
-     <Card body inverse className="card text-center">
-       <CardImg className="cardImage" src={require("../images/" + this.props.project.img)} alt="Card image cap" />
-       <CardImgOverlay>
-            <CardText className="text">
-                <h3>Your Passage</h3>
-                <hr/>
-                <h4>{this.props.project.text}</h4>
-                <Button className="analysisButton">
-                  View Detailed Analysis
-                </Button>
-             </CardText>
-         </CardImgOverlay>
-         <CardFooter>
-           <div className="summary">
-             <h3>Summary</h3>
-             <hr/>
-             <ul>
-             {this.props.project.tones.map(tone =>
-               <li>
-                 <em>{tone.tone_name} </em>
-                 <Emoji symbol={symbolMap(tone)} />
-                 <p> Score: {tone.score}</p>
-               </li>)}
-             </ul>
-           </div>
-           </CardFooter>
-     </Card>
-   </div>
- )
- }
+      <div className="App-header">
+          <h2 className='cardTitle'><em>{this.props.project.title}</em></h2>
+         <Card body inverse className="card text-center">
+           <CardImg className="cardImage" src={require("../images/" + this.props.project.img)} alt="Card image cap" />
+           <CardImgOverlay>
+                <CardText className="text">
+                    <h3>Your Passage</h3>
+                    <hr/>
+                    <h4>{this.props.project.text}</h4>
+                    <Button className="analysisButton">
+                      View Detailed Analysis
+                    </Button>
+                 </CardText>
+             </CardImgOverlay>
+             <CardFooter>
+               <div className="summary">
+                 <h3>Summary</h3>
+                 <hr/>
+                 <ul>
+                 {this.props.project.tones.map(tone =>
+                   <li>
+                     <em>{tone.tone_name} </em>
+                     <Emoji symbol={symbolMap(tone)} />
+                     <p> Score: {tone.score}</p>
+                   </li>)}
+                 </ul>
+               </div>
+            </CardFooter>
+         </Card>
+       </div>
+     )
+   }
 }
