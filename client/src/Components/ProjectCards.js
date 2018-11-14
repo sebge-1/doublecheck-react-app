369c0680402ee333ebede80804c6ce4c50f061af
Sebastian <sebgertz@gmail.com>
2018-11-13 22:10:18 -0500

    feat: ProjectCards now imports ModalButton component

--- client/src/Components/ProjectCards.js
+++ client/src/Components/ProjectCards.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import {CardDeck, Card, CardTitle, CardImg, CardBody, CardText, Button } from 'reactstrap';
+import ModalButton from './ModalButton.js'
+import {CardDeck, Card, CardTitle, CardImg, CardBody, CardText } from 'reactstrap';
 
 const ProjectCards = (props) => (
   <CardDeck>
     {props.projects.map(project => (
       <Card>
-        {/* <CardImg top width="80%" src={require("../images/" + project.image)} alt="Card image cap" /> */}
+        <CardImg top width="80%" src={require("../images/" + project.img)} alt="Card image cap" />
         <CardBody>
-          {/* <CardTitle className="text-center text-muted"><em>{projet.title}</em></CardTitle> */}
+          <CardTitle className="text-center text-muted"><em>{project.title}</em></CardTitle>
           <CardText>{project.text}</CardText>
-           <Button className="showButton btn-sm">See Details</Button>
+           <div className="text-center">
+             <ModalButton defaultState={false} project={project}/>
+          </div>
        </CardBody>
      </Card>
    )
