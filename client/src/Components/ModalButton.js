dd0ddee568bf0ba486a72ea6ef9b73e7b91a54a0
Sebastian <sebgertz@gmail.com>
2018-11-15 19:57:33 -0500

    refactor: extract modal from button

--- client/src/Components/ModalButton.js
+++ client/src/Components/ModalButton.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
-import Emoji from './Emoji.js'
-import { symbolMap } from '../emojis.js';
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Button } from 'reactstrap';
+import ShowModal from './ShowModal.js'
+import { withRouter } from 'react-router'
 import '../stylesheets/ModalButton.css'
 
-export default class ModalButton extends Component {
+class ModalButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,45 +13,26 @@ export default class ModalButton extends Component {
      this.toggle = this.toggle.bind(this);
   }
 
+
   toggle() {
     this.setState({
       modal: !this.state.modal
-    });
-  }
+    }, () => {
+      if (this.state.modal) {
+        this.props.history.push(`/projects/${this.props.project.id}`)
+      } else {
+        this.props.history.push('/projects')
+      }
+    }
+  )}
+
   render() {
     return (
       <div>
         <Button className="showButton" onClick={this.toggle}>See Details</Button>
-        <Modal isOpen={this.state.modal} toggle={this.toggle}>
-          <ModalHeader toggle={this.toggle}>Summary</ModalHeader>
-          <ModalBody>
-            <ul className='list'>
-            {this.props.project.tones.map(tone =>
-              <li className='list-item'>
-                <em>{tone.tone_name} </em>
-                <Emoji symbol={symbolMap(tone)} />
-                <p> Score: {tone.score}</p>
-              </li>)}
-            </ul>
-          </ModalBody>
-          <ModalHeader close="">Detailed Analysis</ModalHeader>
-          <ModalBody>
-              {this.props.project.sentences.map((sentence,index) =>
-                <>
-                <p>{index + 1 }. {sentence.text}</p>
-                <ul className='list'>{sentence.tones.map(tone =>
-                  <li className='list-item'>
-                    <em>{tone.tone_name}</em>
-                    <Emoji symbol={symbolMap(tone)} />
-                    <p>Score: {tone.score}</p>
-                  </li>
-                  )}
-                </ul>
-              </>
-            )}
-          </ModalBody>
-        </Modal>
+        <ShowModal project={this.props.project} modal={this.state.modal} toggle={this.toggle}/>
       </div>
     )
   }
 }
+export default withRouter(ModalButton);
