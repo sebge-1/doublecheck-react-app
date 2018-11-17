a82f1ce7e69eb22894b430af9c917ab851e73af9
Sebastian <sebgertz@gmail.com>
2018-11-17 02:35:42 -0500

    refactor: changed routing in App.js to render single individual component for details view

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import {Switch, BrowserRouter as Router, Route
 import Home from './Components/Home.js'
 import ProjectsContainer from './Containers/ProjectsContainer.js'
 import ResultsContainer from './Containers/ResultsContainer.js'
+import DetailsContainer from './Components/DetailsContainer.js'
 import { fetchProjects } from './Actions/index.js'
-import { connect } from "react-redux";                                                                                                                  
+import { connect } from "react-redux";
+import NavBar from './Components/NavBar.js'                                                                                                              
 import './stylesheets/App.css';
 
 class App extends Component {
@@ -15,24 +17,28 @@ class App extends Component {
 
   render() {
     return (
+
       <Router>
-        <Switch>
-          <Route exact path='/' render={(props) => <Home {...props} buttonVisible={true} />}/>
-          <Route exact path='/projects' render={(props) => <ProjectsContainer {...props} projects={this.props.projectList} />} />
-          <Route path='/projects/:projectId/result' component={ResultsContainer} />
-          <Route exact path='/projects/new' render={(props) => (<div><Home {...props} buttonVisible={false}/></div>)}/>
-        </Switch>
+        <div>
+          <NavBar/>
+          <Switch>
+            <Route exact path='/' render={(props) => <Home {...props} buttonVisible={true} />}/>
+            <Route exact path='/projects/new' render={(props) => (<div><Home {...props} buttonVisible={false}/></div>)}/>
+            <Route exact path='/projects' render={(props) => <ProjectsContainer {...props} projects={this.props.projectList} />} />
+            <Route path='/projects/:id' render={(props) => <DetailsContainer {...props} projects={this.props.projectList} /> }/>
+            <Route path='/projects/:projectId/result' component={ResultsContainer} />
+          </Switch>
+        </div>
       </Router>
+
     );
   }
 }
 
-
-// mapStateToProps is a shorthand to access one slice of the global state of the app (kept in the redux store) via props
 const mapStateToProps = state => {
   return { projectList: state.projects }
 }
-// mapDispatchToProps is a shorthand that lets the app component dispatch an action via props
+
  const mapDispatchToProps = dispatch => ({
   fetchProjects: () => dispatch(fetchProjects())
 })
