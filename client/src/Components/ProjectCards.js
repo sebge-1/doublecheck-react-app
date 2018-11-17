e39a4c888116a6170f9eefe4f7baf0b1bba61431
Sebastian <sebgertz@gmail.com>
2018-11-17 02:42:59 -0500

    refactor: ProjectCards includes link rather than modal button

--- client/src/Components/ProjectCards.js
+++ client/src/Components/ProjectCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ModalButton from './ModalButton.js'
 import {CardDeck, Card, CardTitle, CardImg, CardBody, CardText } from 'reactstrap';
-import {Route, withRouter} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
+import Result from './Result.js'
 import { connect } from 'react-redux';
 
 const ProjectCards = (props) => (
@@ -13,26 +13,17 @@ const ProjectCards = (props) => (
           <CardBody>
             <CardTitle className="text-center text-muted"><em>{project.title}</em></CardTitle>
             <CardText>{project.text}</CardText>
-             <div className="text-center">
-               <ModalButton defaultState={false} project={project}/>
-            </div>
-         </CardBody>
+            <div className="text-center">
+            <Link to={`/projects/${project.id}`}>
+              See Details
+            </Link>
+          </div>
+          </CardBody>
        </Card>
       )
      )}
    </CardDeck>
-     <Route path={"projects/:id"} render={(props, {match}) => <ModalButton {...props} match={match} defaultState={true} project={props.selectedProject} /> } />
- </>
+</>
 )
 
-const mapStateToProps = (state, ownProps) => {
-  const selectedProject = state.projects.find(project => project.id === +ownProps.match.params.id)
-
-    if (selectedProject) {
-      return { selectedProject }
-    } else {
-      return { project: {} }
-    }
-}
-
-export default withRouter(connect(mapStateToProps)(ProjectCards))
+export default ProjectCards
