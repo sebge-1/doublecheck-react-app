9913649034ad7da26b73101c6de403c1dc7476ae
Sebastian <sebgertz@gmail.com>
2018-11-21 20:50:43 -0500

    refactor: DetailsContainer passes selectedProject directly to Results component

--- client/src/Containers/DetailsContainer.js
+++ client/src/Containers/DetailsContainer.js
@@ -1,42 +1,23 @@
 import React, {Component} from 'react'
-import ShowPage from '../Components/ShowPage.js'
+import Result from '../Components/Result.js'
 import {connect} from 'react-redux'
 import '../stylesheets/ModalButton.css'
-import { fetchProjects } from '../Actions/index.js'
 
 class DetailsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projectId : props.match.params.id
-    };
-  }
-
-  // may be necessary to refetch project and store them in redux store:
-  // componentDidMount() {
-  //   console.log(this.props.projects)
-  //   if (this.props.projects === 0) {
-  //     this.props.fetchProjects();
-  //   }
-  // }
-
   render() {
     return (
-        <>
-        <div>{this.props.match.id}</div>
-        <ShowPage projectId={this.state.projectId} projects={this.props.projects}/>
+      <>
+        <Result project={this.props.selectedProject}/>
       </>
     )
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return {projects: state.projects}
-// }
-//
-//  const mapDispatchToProps = dispatch => ({
-//   fetchProjects: () => dispatch(fetchProjects())
-// })
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(DetailsContainer)
-export default DetailsContainer
+const mapStateToProps = (state, ownProps) => {
+  const selectedProject = state.projects.find(project => project.id === +ownProps.match.params.id)
+  if (selectedProject) {
+    return { selectedProject }
+  }
+}
+
+export default connect(mapStateToProps)(DetailsContainer)
