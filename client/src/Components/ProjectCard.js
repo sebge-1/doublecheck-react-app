363c47f01c505289ca4bbfa0f0da34495bd85faf
Sebastian <sebgertz@gmail.com>
2018-12-01 13:10:53 -0500

    separated Card element into separate component

---
+++ client/src/Components/ProjectCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Card, CardTitle, CardImg, CardBody, CardText, Button } from 'reactstrap';
+import {FaTrash} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const ProjectCard = (props) =>
+  <Card key={props.project.id}>
+    <CardImg top width="80%" src={require("../images/" + props.project.img)} alt="Card image cap" />
+    <CardBody>
+    <CardTitle className="text-center text-muted"><em>{props.project.title}</em></CardTitle>
+    <CardText>{props.project.text}</CardText>
+      <div className="text-center">
+        <Link to={`/projects/${props.project.id}`}>
+          See Details
+        </Link>
+      </div>
+      <Button style={{backgroundColor: 'white', border: 'none'}}>
+        <FaTrash onClick={() => props.delete(props.project)} style={{color: 'black'}} />
+      </Button>
+    </CardBody>
+  </Card>
+
+export default ProjectCard
--- client/src/Components/ProjectCards.js
+++ client/src/Components/ProjectCards.js
@@ -1,31 +1,14 @@
 import React from 'react'
-import {CardDeck, Card, CardTitle, CardImg, CardBody, CardText, Button } from 'reactstrap';
-import {Link} from 'react-router-dom';
-import {FaTrash} from 'react-icons/fa';
+import { CardDeck } from 'reactstrap';
+import ProjectCard from './ProjectCard.js'
 
 const ProjectCards = props => (
-  <>
     <CardDeck>
       {props.projects.map(project => (
-        <Card key={project.id}>
-          <CardImg top width="80%" src={require("../images/" + project.img)} alt="Card image cap" />
-          <CardBody>
-            <CardTitle className="text-center text-muted"><em>{project.title}</em></CardTitle>
-            <CardText>{project.text}</CardText>
-            <div className="text-center">
-            <Link to={`/projects/${project.id}`}>
-              See Details
-            </Link>
-          </div>
-          <Button style={{backgroundColor: 'white', border: 'none'}}>
-            <FaTrash onClick={() => props.delete(project)} style={{color: 'black'}} />
-          </Button>
-          </CardBody>
-       </Card>
+        <ProjectCard project={project} delete={props.delete}/>
       )
      )}
    </CardDeck>
-</>
 )
 
 
