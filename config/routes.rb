1ff94879a03a7c7d8a27e86fbb9fa6febf3cb4ac
Sebastian <sebgertz@gmail.com>
2019-01-11 16:07:40 -0500

    add fall-back route to hand Rails routing over to React

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/api/projects', to: 'projects#index'
   post '/api/save', to: 'projects#create', :defaults => {:format => :json}
   delete '/api/projects/:id', to: 'projects#delete'
+  get '*path', to: "pages#index", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
